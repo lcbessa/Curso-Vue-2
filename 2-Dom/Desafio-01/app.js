1c100d3e38a0f067f987371c1c5dd35f4d8a054d
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-05-16 21:37:05 -0300

    Seção 02: Aula 08 - Hora de Praticar - Template (Desafio)

---
+++ 2-Dom/Desafio-01/app.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: "#desafio",
+  data: {
+    nome: "Luiz Carlos Bessa de Lima",
+    idade: 25,
+    imagem: "./Imagem/imagem.jpeg",
+  },
+  methods: {
+    func: () => Math.random(),
+  },
+});
