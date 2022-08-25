6e64d49c218b86b28aaabdfa506af828b708ff96
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-08-25 17:47:23 -0300

    Seção 04: Aula 46 - Projeto#01 - Implementando Atacar

--- 4-Projeto-01-monstro/app.js
+++ 4-Projeto-01-monstro/app.js
@@ -16,6 +16,25 @@ new Vue({
       this.playerLife = 100;
       this.monsterLife = 100;
     },
+    attack(especial) {
+      this.hurt("playerLife", 5, 10, especial);
+      this.hurt("monsterLife", 7, 12, false);
+    },
+    hurt(prop, min, max, especial) {
+      const plus = especial ? 5 : 0;
+      const hurt = this.getRandom(min + plus, max + plus);
+      this[prop] = Math.max(this[prop] - hurt, 0);
+    },
+    getRandom(min, max) {
+      const value = Math.random() * (max - min) + min;
+      return Math.round(value);
+    },
   },
-  watch: {},
+  watch: {
+    hasResult(value) {
+      if (value) {
+        this.running = false;
+      }
+    },
+  },
 });
