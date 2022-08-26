38ac8c3a06025914f07abfa2b8dde25c943a6bd9
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-08-26 10:30:01 -0300

    correção

--- 4-Projeto-01-monstro/app.js
+++ 4-Projeto-01-monstro/app.js
@@ -17,8 +17,8 @@ new Vue({
       this.monsterLife = 100;
     },
     attack(especial) {
-      this.hurt("playerLife", 5, 10, false);
-      this.hurt("monsterLife", 7, 12, especial);
+      this.hurt("playerLife", 7, 12, false);
+      this.hurt("monsterLife", 5, 10, especial);
     },
     hurt(prop, min, max, especial) {
       const plus = especial ? 5 : 0;
