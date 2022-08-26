3467963a9c305286e184324ec38dbc4a4bc6c79d
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-08-26 14:55:40 -0300

    Seção 04: Aula 47 - Projeto#01 - Adicionando o Registro de Log #02

--- 4-Projeto-01-monstro/app.js
+++ 4-Projeto-01-monstro/app.js
@@ -16,6 +16,7 @@ new Vue({
       this.running = true;
       this.playerLife = 100;
       this.monsterLife = 100;
+      this.logs = [];
     },
     attack(especial) {
       this.hurt("monsterLife", 5, 10, especial, "Jogador", "Monstro", "player");
@@ -27,15 +28,16 @@ new Vue({
       const plus = especial ? 5 : 0;
       const hurt = this.getRandom(min + plus, max + plus);
       this[prop] = Math.max(this[prop] - hurt, 0);
-      this.registerLog(`${source} atingiu ${target} com ${hurt}`, cls);
+      this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls);
     },
     healAndHurt() {
       this.heal(10, 15);
-      this.hurt("playerLife", 7, 12, false);
+      this.hurt("playerLife", 7, 12, false, "Monstro", "Jogador", "monster");
     },
     heal(min, max) {
       const heal = this.getRandom(min, max);
       this.playerLife = Math.min(this.playerLife + heal, 100);
+      this.registerLog(`Jogador ganhou força de ${heal}.`, "player");
     },
     getRandom(min, max) {
       const value = Math.random() * (max - min) + min;
