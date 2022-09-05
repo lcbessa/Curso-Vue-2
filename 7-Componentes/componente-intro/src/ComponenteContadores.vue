845de926e61ba362f8fa63c2e5c47d0433991803
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-09-05 17:05:48 -0300

    Seção 07: Aula 76 - Usando Componentes

---
+++ 7-Componentes/componente-intro/src/ComponenteContadores.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="contadores">
+      <app-contador v-for="c in 5" :key="c" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ComponenteContador from "./ComponenteContador.vue";
+export default {
+  components: {
+    "app-contador": ComponenteContador,
+  },
+};
+</script>
+
+<style>
+.contadores {
+  background: #fff;
+}
+</style>
--- 7-Componentes/componente-intro/src/main.js
+++ 7-Componentes/componente-intro/src/main.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import ComponenteContador from "./ComponenteContador.vue";
+import ComponenteContadores from "./ComponenteContadores.vue";
 
 Vue.config.productionTip = false;
-Vue.component("app-contador", ComponenteContador);
+Vue.component("app-contadores", ComponenteContadores);
 
 new Vue({
   render: (h) => h(App),
