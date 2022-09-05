427b12fd94805169b2f3a1696101cc22a82d423f
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-09-05 16:46:48 -0300

    Seção 07: Aula 75 - Criar um Componente

--- 7-Componentes/componente-intro/src/main.js
+++ 7-Componentes/componente-intro/src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import ComponenteContador from "./ComponenteContador.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component("app-contador", ComponenteContador);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
