b0071b7c34c0bb56d67291061e8cd739a62b7a84
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-09-13 11:16:59 -0300

    Seção 07: Aula 78 - Hora de Praticar: Organizar Projeto em Componentes (Desafio)

---
+++ 7-Componentes/Desafio-06/componente-desafio/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <app-menu />
+    <app-toolbar />
+    <v-content>
+      <app-carousel />
+    </v-content>
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import Menu from "./ComponenteMenu.vue";
+import Toolbar from "./ComponenteToolbar.vue";
+import Carousel from "./ComponenteCarousel.vue";
+import Footer from "./ComponenteFooter.vue";
+
+export default {
+  components: {
+    "app-footer": Footer,
+    "app-carousel": Carousel,
+    "app-toolbar": Toolbar,
+    "app-menu": Menu,
+  },
+};
+</script>
