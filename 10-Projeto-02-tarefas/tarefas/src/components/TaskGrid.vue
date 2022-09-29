df1d7e4b3d3c2d563ee34296cfd03830e2ed2431
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-09-29 13:02:36 -0300

    Seção 10:  156. Componente Lista de Tarefas #01

---
+++ 10-Projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tasks">
+    <template>
+      <div v-for="task in tasks" :key=task.name>{{task.name}}</div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.tasks {
+  background: red;
+}
+</style>
