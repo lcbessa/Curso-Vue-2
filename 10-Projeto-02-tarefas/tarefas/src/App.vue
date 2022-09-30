cd4a7a7badd77fc37ff6ff5ff61b0391a83ec452
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-09-30 12:31:09 -0300

    Seção 10: 164. Usando Local Storage

--- 10-Projeto-02-tarefas/tarefas/src/App.vue
+++ 10-Projeto-02-tarefas/tarefas/src/App.vue
@@ -33,6 +33,14 @@ export default {
       return Math.round((done / total) * 100) || 0;
     },
   },
+  watch: {
+    tasks: {
+      deep: true,
+      handler() {
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      },
+    },
+  },
   methods: {
     addTask(task) {
       const sameName = (t) => t.name === task.name;
@@ -51,6 +59,15 @@ export default {
     toggleTaskState(i) {
       this.tasks[i].pending = !this.tasks[i].pending;
     },
+  },
+  created() {
+    const json = localStorage.getItem("tasks");
+    const array = JSON.parse(json);
+    if (Array.isArray(array)) {
+      this.tasks = JSON.parse(json);
+    } else {
+      this.tasks = [];
+    }
   },
 };
 </script>
