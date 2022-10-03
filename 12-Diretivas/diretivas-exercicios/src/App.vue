4b411bd20c3faadcf94fe94b958e9c67ed61f035
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-10-03 17:22:22 -0300

    Seção 12: 190. Registrando Diretivas Localmente

--- 12-Diretivas/diretivas-exercicios/src/App.vue
+++ 12-Diretivas/diretivas-exercicios/src/App.vue
@@ -6,11 +6,33 @@
     <hr />
     <p v-destaque:fundo.atrasar="'lightblue'">Usando Diretiva Personalizada</p>
     <p v-destaque="cor">Usando Diretiva Personalizada</p>
+    <hr />
+    <p v-destaque-local:fundo.atrasar="'lightblue'">
+      Usando Diretiva Personalizada
+    </p>
+    <p v-destaque-local="cor">Usando Diretiva Personalizada</p>
   </div>
 </template>
 
 <script>
 export default {
+  components: {},
+  directives: {
+    "destaque-local": {
+      bind(el, binding) {
+        // el.style.backgroundColor = "lightgreen";
+        let atraso = 0;
+        if (binding.modifiers["atrasar"]) atraso = 3000;
+        setTimeout(() => {
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }, atraso);
+      },
+    },
+  },
   data() {
     return {
       cor: "red",
