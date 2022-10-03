2f976f09400e17091025439494211c1e7a9e5401
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-10-03 16:22:23 -0300

    Seção 12: 185. Criando uma Diretiva Simples

---
+++ 12-Diretivas/diretivas-exercicios/src/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+Vue.directive("destaque", {
+  bind(el) {
+    el.style.backgroundColor = "lightgreen";
+  },
+});
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
