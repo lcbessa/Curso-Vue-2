d247bf90f328f2cd0b87fcb5c646ada302e422ef
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-10-03 18:50:10 -0300

    Seção 12: 192. Passando Valores mais Complexos para as Diretivas

--- 12-Diretivas/diretivas-exercicios/src/App.vue
+++ 12-Diretivas/diretivas-exercicios/src/App.vue
@@ -7,10 +7,24 @@
     <p v-destaque:fundo.atrasar="'lightblue'">Usando Diretiva Personalizada</p>
     <p v-destaque="cor">Usando Diretiva Personalizada</p>
     <hr />
-    <p v-destaque-local:fundo.atrasar="'lightblue'">
+    <p
+      v-destaque-local:fundo.atrasar.alternar="{
+        cor1: 'green',
+        cor2: 'red',
+        atraso: 2000,
+        intervalo: 200,
+      }"
+    >
       Usando Diretiva Personalizada
     </p>
-    <p v-destaque-local="cor">Usando Diretiva Personalizada</p>
+    <p
+      v-destaque-local.atrasar="{
+        cor1: 'red',
+        atraso: 5000,
+      }"
+    >
+      Usando Diretiva Personalizada
+    </p>
   </div>
 </template>
 
@@ -20,14 +34,28 @@ export default {
   directives: {
     "destaque-local": {
       bind(el, binding) {
-        // el.style.backgroundColor = "lightgreen";
-        let atraso = 0;
-        if (binding.modifiers["atrasar"]) atraso = 3000;
-        setTimeout(() => {
+        const aplicarCor = (cor) => {
           if (binding.arg === "fundo") {
-            el.style.backgroundColor = binding.value;
+            el.style.backgroundColor = cor;
           } else {
-            el.style.color = binding.value;
+            el.style.color = cor;
+          }
+        };
+        let atraso = 0;
+        if (binding.modifiers["atrasar"]) atraso = binding.value.atraso;
+
+        const cor1 = binding.value.cor1;
+        const cor2 = binding.value.cor2;
+        let corAtual = cor1;
+
+        setTimeout(() => {
+          if (binding.modifiers["alternar"]) {
+            setInterval(() => {
+              corAtual = corAtual === cor1 ? cor2 : cor1;
+              aplicarCor(corAtual);
+            }, 1000);
+          } else {
+            aplicarCor(binding.value.cor1);
           }
         }, atraso);
       },
