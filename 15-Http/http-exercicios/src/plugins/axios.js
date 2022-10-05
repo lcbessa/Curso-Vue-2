28846ccfb8da503253c6952e0647bb47421aff5b
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-10-05 17:00:59 -0300

    Seção 15:  245. Adicionando Headers Globais

--- 15-Http/http-exercicios/src/plugins/axios.js
+++ 15-Http/http-exercicios/src/plugins/axios.js
@@ -2,12 +2,17 @@ import Vue from "vue";
 import axios from "axios";
 
 // axios.defaults.baseURL = "https://curso-vue-5313b-default-rtdb.firebaseio.com/";
+// axios.defaults.headers.common["Authorization"] = "abc123";
+// axios.defaults.headers.get["Accepts"] = "application/json";
 
 Vue.use({
   install(Vue) {
     // Vue.prototype.$http = axios;
     Vue.prototype.$http = axios.create({
       baseURL: "https://curso-vue-5313b-default-rtdb.firebaseio.com/",
+      headers: {
+        Authorization: "abc123",
+      },
     });
     Vue.prototype.$http.interceptors.request.use(
       (config) => {
