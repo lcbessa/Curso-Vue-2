d5bdc733c59f2c62ddc9cf6711f130bb78caa09e
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-10-06 17:03:14 -0300

    Seção 16: 263. Tornando Router Links mais Dinâmico

--- 16-Rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ 16-Rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="usuario-detalhe">
     <h3>Usuário Detalhe</h3>
-    <hr />
     <p><strong>Código:</strong> {{ id }}</p>
+    <router-link tag="button" primario :to="`/usuario/${id}/editar`"
+      >Editar</router-link
+    >
   </div>
 </template>
 
