ba26275dcb1297f0a20bded8d4a601813ce6abb9
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-10-06 14:34:44 -0300

    Seção 16: 257. Configurando Parâmetros de Rotas

--- 16-Rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ 16-Rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="usuario">
     <h2>Usuários</h2>
+    <hr />
+    <button sucesso @click="irParaIncio">Voltar</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    irParaIncio() {
+      this.$router.push({ path: "/" });
+    },
+  },
+};
 </script>
 
 <style>
