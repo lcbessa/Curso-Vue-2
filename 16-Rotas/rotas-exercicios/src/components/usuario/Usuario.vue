998d33766d0c99ee8fed9bc9448cdc1062dcae8b
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-10-06 16:44:10 -0300

    Seção 16: 261. Configurando Rotas Filhas (Rotas Aninhadas)

--- 16-Rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ 16-Rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,24 +2,13 @@
   <div class="usuario">
     <h2>Usuários</h2>
     <hr />
-    <p><strong>Código:</strong> {{ id }}</p>
+    <router-view></router-view>
     <button sucesso @click="irParaIncio">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["id"],
-  //   data() {
-  //     return {
-  //       id: this.$route.params.id,
-  //     };
-  //   },
-  //   watch: {
-  //     $route(to, from) {
-  //       this.id = to.params.id;
-  //     },
-  //   },
   methods: {
     irParaIncio() {
       this.$router.push({ path: "/" });
