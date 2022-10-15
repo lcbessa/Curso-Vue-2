0e1135c6a63d2ffa14d21d6ae8b3f06b23a2d459
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-10-15 15:33:49 -0300

    Seção 18: 308. Criando o Componente de Ações (Stocks)

--- 18-Projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
+++ 18-Projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Ações</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" />
+  </v-layout>
 </template>
 
 <script>
-export default {};
+import Stock from "./Stock.vue";
+export default {
+  components: {
+    Stock,
+  },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 110 },
+        { id: 2, name: "Google", price: 280 },
+        { id: 3, name: "Apple", price: 200 },
+        { id: 4, name: "Twitter", price: 83 },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
