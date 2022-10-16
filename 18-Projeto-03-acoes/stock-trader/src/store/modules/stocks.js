cbd76d1788c04fdc14867d9af519611da1c0c952
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-10-16 18:06:25 -0300

    Seção 18:  317. Finalizando Dia - Alterando Preço das Ações

--- 18-Projeto-03-acoes/stock-trader/src/store/modules/stocks.js
+++ 18-Projeto-03-acoes/stock-trader/src/store/modules/stocks.js
@@ -8,6 +8,11 @@ export default {
     setStocks(state, stocks) {
       state.stocks = stocks;
     },
+    ramdomizeStocks(state) {
+      state.stocks.forEach((stock) => {
+        stock.price = Math.round(stock.price * (1 + Math.random() - 0.42));
+      });
+    },
   },
   actions: {
     buyStock({ commit }, order) {
@@ -15,6 +20,9 @@ export default {
     },
     initStocks({ commit }) {
       commit("setStocks", stocks);
+    },
+    ramdomizeStocks({ commit }) {
+      commit("ramdomizeStocks");
     },
   },
   getters: {
